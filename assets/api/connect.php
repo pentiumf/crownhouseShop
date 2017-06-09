0d1d0c61806ef4c0b66bb39cec22ace1c452222d
Nick Sw <swaggarnick@gmail.com>
2017-06-09 02:26:21 +0000

    database added

--- assets/api/connect.php
+++ assets/api/connect.php
@@ -1,8 +1,17 @@
 <?php
 
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'crownhouse';
+//$server = 'localhost';
+//$username = 'root';
+//$password = '';
+//$dbname = 'crownhouse';
+//
+//$dbcon = new mysqli($server, $username, $password, $dbname);
 
-$dbcon = new mysqli($server, $username, $password, $dbname);
+
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$dbcon = new mysqli($server, $username, $password, $db);
